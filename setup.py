0759b99e0c1ef585088ecf1d9f119c2a287603ab
Christoph Scheid <christoph.scheid@c3s.cc>
2013-07-23 11:50:33 +0000

    initial from scaffold: pcreate -t alchemy c3sbrainz

---
+++ setup.py
@@ -0,0 +1,44 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'pyramid',
+    'SQLAlchemy',
+    'transaction',
+    'pyramid_tm',
+    'pyramid_debugtoolbar',
+    'zope.sqlalchemy',
+    'waitress',
+    ]
+
+setup(name='c3sbrainz',
+      version='0.0',
+      description='c3sbrainz',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='c3sbrainz',
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = c3sbrainz:main
+      [console_scripts]
+      initialize_c3sbrainz_db = c3sbrainz.scripts.initializedb:main
+      """,
+      )
